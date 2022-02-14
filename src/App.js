2087f61c62469c2d2c66b2dda2b2ab8cf26b7fae
jonher16 <1999.jon@gmail.com>
2022-02-14 20:24:28 +0100

    Added login system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="root">
       <h1 className="mt-3">Coinex</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
